0697adfd5bc13e7af260df5dba0168031a7e073d
qe345 <bzxcooptv5554@protonmail.com>
2025-04-10 03:58:01 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || '64e535e5-1895-4493-a44a-ad50515d14f5'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '78611a3f-009a-4086-83f3-a7a385504762'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'mvbcbcvv33.thasdfg3.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzQzYTJhMWEyZGRkMzg2MjhiYjU1NWZmZmUwZDJmNjciLCJ0IjoiNWFlNjc1OWItMDYwMS00MGE5LTljYzktMTk5NzdkY2JmNzNlIiwicyI6IlpUaGxNalpqTTJNdE9XSXpZaTAwWXpNeUxXSTBNVGN0WkRFeE5EZGhZell4T1RobSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo-3.ljqwert.dpdns.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzQzYTJhMWEyZGRkMzg2MjhiYjU1NWZmZmUwZDJmNjciLCJ0IjoiN2IwYTI3MTEtYjkyNi00NzMzLWExMzQtNmE3MTIxZDU4NDQyIiwicyI6Ik1qQTFPREppT0dJdFltUmtOeTAwTURsbUxUa3hOVFF0WkRabFlUTm1aR000TW1SbSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
